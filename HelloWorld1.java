b8ddf8f3d6c7e648112c2cb8a0424460620921c9
viilupk <kadi.viilup@gmail.com>
2016-10-01 13:39:07 +0300

    Add files via upload

---
+++ HelloWorld1.java
@@ -0,0 +1,10 @@
+package com.exampla.helloworld;
+
+/**
+ * Created by Admin on 1.10.2016.
+ */
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
